0b2e9e10dda47c871d758e57f91916d62a971286
Luka5525 <luka.durasinovic@student.manchester.ac.uk>
2022-12-01 14:35:27 +0000

    Debug

--- software/main.py
+++ software/main.py
@@ -1,19 +1,21 @@
 from lzma import MF_BT3
 import sys, os
 sys.path.append(os.getcwd() + "\\motion") #add function folder to path
-sys.path.append(os.getcwd() + "\\laser_tracking") #add function folder to path
+# sys.path.append(os.getcwd() + "\\laser_tracking") #add function folder to path
 import motion_functions as mf
-import laser_tracking as lt
+# import laser_tracking as lt
 
 def main():
-	debug=1
+	debug=0
 	if debug==1:
 		[test1, test2] = mf.import_test
 		print(test1)
 		print(test2)
 	
 	velocity_publisher, vel_msg = mf.init()
-	angle_deg, distance = lt.find_laser()	### RECEIVE COMMAND FROM LASER TRACKER
+	# angle_deg, distance = lt.find_laser()	### RECEIVE COMMAND FROM LASER TRACKER
+	angle_deg = 40
+	distance = 3
 	mf.send_motion_command(vel_msg, velocity_publisher, angle_deg, distance)
 
 
