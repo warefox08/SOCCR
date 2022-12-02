42aee3c35dcac3c207ae4c8a8546d15840e6981a
Luka5525 <luka.durasinovic@student.manchester.ac.uk>
2022-12-02 13:42:56 +0000

    Comms debug

--- software/comms/listener.py
+++ software/comms/listener.py
@@ -4,12 +4,14 @@ from std_msgs.msg import String
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    return data.data
     
 def listener():
 
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("Team10/Execute", String, callback)
+    
 
     # rospy.spin()
 
