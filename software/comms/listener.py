ea63647f1f6cd11bb3551b7fb3241753a97e990f
Luka5525 <luka.durasinovic@student.manchester.ac.uk>
2022-12-05 10:28:53 +0000

    Syntax changes to listener.py

--- software/comms/listener.py
+++ software/comms/listener.py
@@ -4,12 +4,13 @@ import paho.mqtt.client as mqtt
 from std_msgs.msg import String
 
 def on_message(client, userdata, msg):
-   command_received_from_microcontroller=true
-   return command_received_from_microcontroller
+   return True
 
-client = mqtt.Client()    
-client.on_message = on_message
-client.connect(192.168.43.221, 1883, 60)
+def init():
+   client = mqtt.Client()    
+   client.on_message = on_message
+   client.connect("192.168.43.221", 1883, 60)
 
 if __name__ == '__main__':
-    listener()
+    init()
+    on_message()
