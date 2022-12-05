5b75f202cfccf08e219ec8e8c85cad3956d0af08
TrajcheBojadjiev <114410190+TrajcheBojadjiev@users.noreply.github.com>
2022-12-05 10:23:47 +0000

    Update listener.py

--- software/comms/listener.py
+++ software/comms/listener.py
@@ -3,12 +3,12 @@ import rospy
 import paho.mqtt.client as mqtt
 from std_msgs.msg import String
 
-def on_message(client, userdata, msg):
+def onmsg(client, userdata, msg):
    command_received_from_microcontroller=true
    return command_received_from_microcontroller
 
 client = mqtt.Client()    
-client.on_message = on_message
+client.on_message = onmsg
 client.connect(192.168.43.221, 1883, 60)
 
 if __name__ == '__main__':
