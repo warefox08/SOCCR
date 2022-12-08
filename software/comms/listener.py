efa038bf9ec1f66efccd0ae75fd95e5b57db32b8
Trey <trajche.bojadjiev@student.manchester.ac.uk>
2022-12-08 16:13:49 +0000

    Push Comms Fix

--- software/comms/listener.py
+++ software/comms/listener.py
@@ -2,13 +2,15 @@
 import rospy
 from rosserial_python import SerialClient, RosSerialServer
 from std_msgs.msg import String
+def callback():
+    print("Message Received")
 
 def init():
-    rospy.init_node("listen")
-    rospy.loginfo("ROS Serial Listener")
+    rospy.init_node("Team10")
     port_name = rospy.get_param('~tcp_port','tcp')
     baud = int(rospy.get_param('~baud','57600'))
-
+    rospy.Subscriber("Team10", String, callback)
+    
 
 if __name__ == '__main__':
     init()
