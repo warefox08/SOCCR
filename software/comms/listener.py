5260d1a97623307b32842e2f61f76145b03cffe7
Luka5525 <luka.durasinovic@student.manchester.ac.uk>
2022-12-08 16:59:21 +0000

    Listener flag debug

--- software/comms/listener.py
+++ software/comms/listener.py
@@ -2,17 +2,17 @@
 import rospy
 from rosserial_python import SerialClient, RosSerialServer
 from std_msgs.msg import String
-def callback(arg, flag):
+def callback(arg):
     print(flag)
     flag = 1
     print(flag)
     print("Message Received")
 
-def init(flag):
+def init():
     rospy.init_node("Team10")
     port_name = rospy.get_param('~tcp_port','tcp')
     baud = int(rospy.get_param('~baud','57600'))
-    rospy.Subscriber("Team10", String, callback, flag)
+    rospy.Subscriber("Team10", String, callback)
     
 
 if __name__ == '__main__':
