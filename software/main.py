5bc95893d674660661578dc9615c39df02fbb1c9
Ashe Kim <ashekim@robotis.com>
2022-12-09 23:24:35 +0900

    listener debug

--- software/main.py
+++ software/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 from lzma import MF_BT3
 import sys, os
-# sys.path.append(os.path.join(os.getcwd(), "motion")) #add function folder to path
+sys.path.append(os.path.join(os.getcwd(), "motion")) #add function folder to path
 # sys.path.append(os.path.join(os.getcwd(), "laser_tracking")) #add function folder to path
-sys.path.append(os.path.join(os.getcwd(), "software/comms"))
-# import motion_functions as mf
+sys.path.append(os.path.join(os.getcwd(), "comms"))
+
+print(sys.path)
+import motion_functions as mf
 # import laser_tracking_functions as lt
 
 import listener_class
@@ -16,7 +18,7 @@ def main():
 	# 	print(test1)
 	# 	print(test2)
 	
-	#velocity_publisher, vel_msg = mf.init()
+	velocity_publisher, vel_msg = mf.init()
 	# [dc, fov_h, res_h, res_v] = lt.init()
 	listener = listener_class.listener()
 	listener.init()
@@ -26,9 +28,9 @@ def main():
 			print("flag raised")
 			listener.lower_flag()
 			# angle_deg, distance = lt.find_vector_to_laser(dc, fov_h, res_h, res_v)
-			#angle_deg = 10
-			#distance = 0.5
-			#mf.send_motion_command(vel_msg, velocity_publisher, angle_deg, distance)
+			angle_deg = 10
+			distance = 0.5
+			mf.send_motion_command(vel_msg, velocity_publisher, angle_deg, distance)
 			
 
 if __name__ == "__main__":
