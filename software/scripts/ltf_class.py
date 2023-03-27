5f3c2f899fbe8c017f546d3c8158a72bf5f859f9
Luka5525 <luka.durasinovic@student.manchester.ac.uk>
2023-03-27 14:39:07 +0100

    chagned wait key and moved to /scripts

--- software/scripts/ltf_class.py
+++ software/scripts/ltf_class.py
@@ -52,24 +52,30 @@ class laser_tracker:
         hsv_frame = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 
         # Define range of red color in HSV -> red hue boundary -- worth testing out and messing around with 
-        lower_red = np.array([30, 150, 50])
-        upper_red = np.array([255, 255, 180])
+        lower_red = np.array([160, 50, 50]) #([30, 150, 50]) - these vals included blue and red 
+        upper_red = np.array([180, 255, 255]) #([255, 255, 180])
 
         # Create a mask using the HSV range
         mask_colour = cv2.inRange(hsv_frame, lower_red, upper_red)
         # Threshold the HSV image to filter out all 'non-red' pixels
-        red_filter = cv2.bitwise_and(color_frame, color_frame, mask = mask_colour)
+        #red_filter = cv2.bitwise_and(color_frame, color_frame, mask = mask_colour)
+        red_filter = cv2.bitwise_and(color_image, color_image, mask = mask_colour)
 
         (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask_colour) #finds the min, max and index values of the mask - useful data for mask tuning
-        # cv2.imshow("red", red_filter)
-        # cv2.imshow("color", color_image)
-        # cv2.imshow("hsv", hsv_frame)
+        
         # Make a binarised version of the image and find the coordinates of non-zero pixels 
 
         img = cv2.cvtColor(red_filter,cv2.COLOR_BGR2GRAY)
         img = img.astype(np.uint8) #converts the image to int type - each pixel has an int value of 0-255 rathe than floating points between 0 and 
 
         points = cv2.findNonZero(img) #finds coordinates of non-zero pixels 
+
+        # show frames for debugging 
+        #cv2.imshow("red", red_filter)
+        #cv2.imshow("color", color_image)
+        #cv2.imshow("hsv", hsv_frame)
+        #cv2.imshow("binary image", img)
+        #cv2.waitKey(0) # frame by frame for debugging 
 
         if (points is not None):
             [angle, distance, distance_x] = self.find_laser_cords(points)
@@ -90,24 +96,27 @@ class laser_tracker:
         p2_o = -p2+240
         deg_per_pixel_h = self.fov_h/self.res_h
         angle_deg_h = (p1-320)*deg_per_pixel_h
+        phi = 90-angle_deg_h
 
         distance = self.depth_msg[p2, p1] #finds the distance to the laser point *NOTE: Distance = depth from camera Plane 
         distance_origin = self.depth_msg[240, 320] #depth of origin pixel 
 
         angle_rad = np.pi*angle_deg_h/180
-        distance_x = np.tan(angle_rad)*distance_origin
+        phi_rad = np.pi*phi/180
+        # distance_x = np.tan(angle_rad)*distance_origin
+        distance_x = np.sin(angle_rad)*(distance/np.sin(phi_rad))
         
-        angle = np.arccos(distance_origin / distance_x) # is this not just returning angle_rad again?
-        angle_d = 180*angle/np.pi
+        # angle = np.arccos(distance_origin / distance_x) # is this not just returning angle_rad again?
+        # angle_d = 180*angle/np.pi
 
         # Math for the generalised version -- NOTE: THIS IS UNTESTED 
         #### care for diag shouldnt this be (l/sin b) or (l/cos theta) since its all in the same plane elevation 
         #### shouldnt be taken into consideration
-        l = distance_origin
-        a = angle_rad 
-        b = (np.pi/2)-a # angle beta = 90 - alpha (fov_h angle) i.e pi/2 - alpha (in rad) 
-        diag = (l)*np.sin(b) # The 'diagonal' distance to the laser point as a function using the sine rule
-        w = (l)*np.sin(a) # The 'width'i.e. distance between the origin point and the laser point (alt to distance_x)
+        # l = distance_origin
+        # a = angle_rad 
+        # b = (np.pi/2)-a # angle beta = 90 - alpha (fov_h angle) i.e pi/2 - alpha (in rad) 
+        # diag = (l)*np.sin(b) # The 'diagonal' distance to the laser point as a function using the sine rule
+        # w = (l)*np.sin(a) # The 'width'i.e. distance between the origin point and the laser point (alt to distance_x)
         
         # log_data(p1, p2, distance_x, angle_deg_h)
 
