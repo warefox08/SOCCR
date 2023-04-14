98e24f9952b3277ad5ae2980411081a11145edbd
TrajcheBojadjiev <trajche.bojadjiev@student.manchester.ac.uk>
2023-04-14 12:09:00 +0100

    SonyTest update

--- MC/SonyTest/Readme.txt
+++
@@ -1,1 +0,0 @@
-This is the .ino fule to be uploaded to the Spresense for use of the GPIO and the TFT Screen
--- MC/SonyTest/SonyTest.ino
+++ MC/SonyTest/SonyTest.ino
@@ -1,70 +1,83 @@
 
-#include <SoftwareSerial.h>
 
+#include <SoftwareSerial.h>
 #include <ros.h>
 #include <std_msgs/String.h>
+
+#include <SPI.h>
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 
-#define REP 14
-#include <SPI.h>
 
-#define PBR 21
-#define PBC 20
-#define PBL 19
+#define PBR 7
+#define PBC 6
+#define PBL 5
 
-#define TFT_CS 24 // Chip Select SPI_CS
-#define TFT_SCLK 23  // Clock out SPI_SCK
-#define TFT_DC 17  //Data Clock SPI_MISO
-#define TFT_MOSI 16  // Data out SPI_MOSI
-#define TFT_RST 15  //Reset PWM
+#define TFT_CS        10
+#define TFT_RST        9 
+#define TFT_DC         8
 
-#define RX 0
-#define TX 1
-SoftwareSerial TRN(RX, TX);
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+//Adafruit_ST7789 tft = Adafruit_ST7789(&SPI4, TFT_CS, TFT_DC, TFT_RST);
+//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+char guipos = 0;
+char* guitext[3] = {"Move", "Interact", "Status"};
+char commandtxt[3] ={'M','I','S'};
+//char* commandtxt[3] = {0, 1, 0};
 
-//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-char guipos =0;
-char* guitext[3] ={"Move","Interact","Status"};
-char* commandtxt[3] ={"M","I","S"};
+bool transmit = false;
+bool buttonpress[3] = {false, false, false};
 
-bool transmit=false;
-
-void left(){
+void left() {
   //Serial.print("Left");
-  if (guipos > 0){
-  tft.setCursor(200, guipos*34);
-  tft.setTextColor(ST77XX_BLACK);
-  tft.print("<");
-  guipos--;
-  tft.setCursor(200, guipos*34);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.print("<");
+  static unsigned long OldIntTimeL;
+  unsigned long IntTimeL = millis();
+  if ((IntTimeL - OldIntTimeL) > 100) {
+    if (guipos > 0) {
+      tft.setCursor(200, guipos * 34);
+      tft.setTextColor(ST77XX_BLACK);
+      tft.print("<");
+      guipos--;
+      tft.setCursor(200, guipos * 34);
+      tft.setTextColor(ST77XX_WHITE);
+      tft.print("<");
+    }
   }
+  OldIntTimeL = IntTimeL;
 }
-void right(){
-  //Serial.print("Right");  
-  if (guipos < (sizeof(guitext) / sizeof(*guitext))-1){
-  tft.setCursor(200, guipos*34);
-  tft.setTextColor(ST77XX_BLACK);
-  tft.print("<");
-  guipos++;
-  tft.setCursor(200, guipos*34);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.print("<");
+void right() {
+  //Serial.print("Right");
+  static unsigned long OldIntTimeR;
+  unsigned long IntTimeR = millis();
+  if ((IntTimeR - OldIntTimeR) > 100) {
+    if (guipos < (sizeof(guitext) / sizeof(*guitext)) - 1) {
+      tft.setCursor(200, guipos * 34);
+      tft.setTextColor(ST77XX_BLACK);
+      tft.print("<");
+      guipos++;
+      tft.setCursor(200, guipos * 34);
+      tft.setTextColor(ST77XX_WHITE);
+      tft.print("<");
+    }
   }
+  OldIntTimeR = IntTimeR;
 }
-void centre(){
-  //Serial.print("Centre \n");  
-  Serial.print(guitext[guipos]);
-  TRN.write(commandtxt[guipos]);
+void centre() {
+  static unsigned long OldIntTimeC;
+  unsigned long IntTimeC = millis();
+  if ((IntTimeC - OldIntTimeC) > 100) {
+    Serial.print(guitext[guipos]);
+    Serial2.write(commandtxt[guipos]);
+  }
+  OldIntTimeC = IntTimeC;
 }
 void setup() {
   Serial.begin(9600);//Serial for debugging
-  TRN.begin(115200); //Serial To ESP32
+  //TRN.begin(115200); //Serial To ESP32
+  Serial2.begin(115200);
   tftsetup();
+
   pinMode(PBL, INPUT);
   pinMode(PBC, INPUT);
   pinMode(PBR, INPUT);
@@ -75,16 +88,16 @@ void setup() {
 }
 
 void loop() {
-  
+
 }
 
-void tftsetup(){
-  tft.init(240, 240);  
+void tftsetup() {
+  tft.init(240, 240);
   tft.fillScreen(ST77XX_BLACK);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(3);
-  for (int i=0;(i < sizeof(guitext) / sizeof(*guitext)); i++){
-    tft.setCursor(0,i*32);
+  for (int i = 0; (i < sizeof(guitext) / sizeof(*guitext)); i++) {
+    tft.setCursor(0, i * 32);
     tft.print(guitext[i]);
   }
 }
