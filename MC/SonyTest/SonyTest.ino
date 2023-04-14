56d19b2126130f2e4b33d9a2a5957b8f3a25c74d
TrajcheBojadjiev <trajche.bojadjiev@student.manchester.ac.uk>
2023-04-14 15:03:46 +0100

    ui Changes

--- MC/SonyTest/SonyTest.ino
+++ MC/SonyTest/SonyTest.ino
@@ -24,11 +24,12 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 //Adafruit_ST7789 tft = Adafruit_ST7789(&SPI4, TFT_CS, TFT_DC, TFT_RST);
 //Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 char guipos = 0;
-char* guitext[4] = {"Move", "Stop", "Right", "Left"};
-char commandtxt[4] = {'M', 'S', 'R', 'L'};
+char* guitext[4] = {"Move", "Right", "Left","Stop"};
+char commandtxt[4] = {'M', 'R', 'L','S'};
 
 bool transmit = false;
 bool buttonpress[3] = {false, false, false};
+bool DF=1;
 
 void guidown() {
   if (guipos > 0) {
@@ -43,7 +44,7 @@ void guidown() {
 }
 
 void guiup() {
-  if (guipos < (sizeof(guitext) / sizeof(*guitext)) - 1) {
+  if (guipos < LENGTH(guitext) - 2) {
     tft.setCursor(200, guipos * 34);
     tft.setTextColor(ST77XX_BLACK);
     tft.print("<");
@@ -79,6 +80,7 @@ void centre() {
   if ((IntTimeC - OldIntTimeC) > 100) {
     Serial.print(guitext[guipos]);
     Serial2.write(commandtxt[guipos]);
+    screenstate(commandtxt[guipos]);
   }
   OldIntTimeC = IntTimeC;
 }
@@ -91,9 +93,10 @@ void setup() {
   pinMode(PBL, INPUT);
   pinMode(PBC, INPUT);
   pinMode(PBR, INPUT);
-  attachInterrupt(PBL, left, FALLING);
-  attachInterrupt(PBC, centre, FALLING);
-  attachInterrupt(PBR, right, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PBL), left, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PBR), right, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PBC), centre, FALLING);
+
   delay(1000);
 }
 
@@ -101,15 +104,51 @@ void loop() {
 
 }
 void screenstate(char ScrSt) {
-
+switch(ScrSt){
+  case 'M':
+    detachInterrupt(digitalPinToInterrupt(PBL));
+    detachInterrupt(digitalPinToInterrupt(PBR));
+    tft.setCursor(200, guipos * 34);
+    tft.setTextColor(ST77XX_BLACK);
+    tft.print("<");
+    guipos=3;
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setCursor(0,0);
+    tft.print("Movement in\nprogress, \npress fire to stop");
+    DF=0;
+    break;
+  default:
+    if (DF==0){
+      attachInterrupt(digitalPinToInterrupt(PBL), left, FALLING);
+      attachInterrupt(digitalPinToInterrupt(PBR), right, FALLING);
+      tft.fillScreen(ST77XX_BLACK);
+      tft.setTextColor(ST77XX_WHITE);
+      guipos=0;
+      tft.setCursor(200, 0);
+      tft.setTextColor(ST77XX_WHITE);
+      tft.print("<");
+      tft.setTextSize(3);
+      for (int i = 0; (i < (LENGTH(guitext)-1)); i++) {
+      tft.setCursor(0, i * 32);
+      tft.print(guitext[i]);
+      }
+    }
+    DF=1;
+    break;
+    }
 }
 void tftsetup() {
   tft.init(240, 240);
   tft.fillScreen(ST77XX_BLACK);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(3);
-  for (int i = 0; (i < LENGTH(guitext)); i++) {
+  for (int i = 0; (i < (LENGTH(guitext)-1)); i++) {
     tft.setCursor(0, i * 32);
     tft.print(guitext[i]);
   }
+  guipos=0;
+  tft.setCursor(200, 0);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print("<");
 }
